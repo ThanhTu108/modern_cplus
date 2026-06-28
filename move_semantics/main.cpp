a3cc718a4e958998f6b48af416a7648f5e9106f6
ThanhTu <thanhtu10803@gmail.com>
2026-06-28 13:12:30 +0700

    basic move semantics

--- move_semantics/main.cpp
+++ move_semantics/main.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
+    std::string str = "hello";
+    std::string new_str;
+    std::cout << "str: " << str << "\n";
+    std::cout << "new_str: " << new_str << "\n";
 
-
+    // new_str = std::move(str);
+    // new_str = static_cast<std::string &&>(str);
+    // new_str
+    std::cout << "str: " << str << "\n";
+    std::cout << "new_str: " << new_str << "\n";
     return 0;
 }
